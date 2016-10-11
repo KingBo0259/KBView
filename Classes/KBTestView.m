e65d8771e0794592789f3926d591d170766d59cf
jinlb <jlb@kuaihuoyun.com>
2016-10-11 10:40:30 +0800

    comment

---
+++ Classes/KBTestView.m
@@ -0,0 +1,13 @@
+//
+//  KBTestView.m
+//  KBView
+//
+//  Created by jinlb on 16/10/11.
+//  Copyright © 2016年 jinlb. All rights reserved.
+//
+
+#import "KBTestView.h"
+
+@implementation KBTestView
+
+@end
