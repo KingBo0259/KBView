03728ee73d94ee54396167bfb18a2d6c9948d284
jinlb <jlb@kuaihuoyun.com>
2016-10-11 10:37:06 +0800

    TestView

---
+++ KBView/KBView/CustomView/KBTestView.h
@@ -0,0 +1,13 @@
+//
+//  KBTestView.h
+//  KBView
+//
+//  Created by jinlb on 16/10/11.
+//  Copyright © 2016年 jinlb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBTestView : UIView
+
+@end
